81854180045f6818c38c98b16bcbe7514c1aea6c
getzch <keepprogress@gmail.com>
2021-04-03 14:12:00 +0800

    complete 303. Building A React App 1

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({ name, email, id}) => {
+    
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'> 
-            <img alt = 'robots' src='https://robohash.org/test?200X200' />
+            <img alt = 'robots' src={`https://robohash.org/${id}?200X200`} />
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
